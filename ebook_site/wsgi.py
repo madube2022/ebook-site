cd287744c38c93f6308f02463dc1d77f863cc204
madube2022 <mabeloluchi@gmail.com>
2025-07-17 11:40:27 +0100

    load .env in wsgi.py

--- ebook_site/wsgi.py
+++ ebook_site/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-from django.core.wsgi import get_wsgi_application
+# Define the base directory (2 levels up from this file)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from the .env file
+load_dotenv(dotenv_path=BASE_DIR / '.env')
+
+# Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebook_site.settings')
 
+# Create the application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
